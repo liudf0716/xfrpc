fadce224061d4b83fa0108fe4606ae0dcfd8705e
Dengfeng Liu <liudf0716@qq.com>
2017-04-27 21:04:56 +0800

    Update msg.h

--- msg.h
+++ msg.h
@@ -23,3 +23,42 @@
     @brief xfrp msg struct
     @author Copyright (C) 2016 Dengfeng Liu <liudengfeng@kunteng.org>
 */
+
+
+struct general_response {
+	int		code;
+	char	*msg;
+};
+
+// messages between control connections of frpc and frps
+type ControlReq struct {
+	Type          int64  `json:"type"`
+	ProxyName     string `json:"proxy_name"`
+	AuthKey       string `json:"auth_key"`
+	UseEncryption bool   `json:"use_encryption"`
+	UseGzip       bool   `json:"use_gzip"`
+	PoolCount     int64  `json:"pool_count"`
+
+	// configures used if privilege_mode is enabled
+	PrivilegeMode     bool     `json:"privilege_mode"`
+	PrivilegeKey      string   `json:"privilege_key"`
+	ProxyType         string   `json:"proxy_type"`
+	RemotePort        int64    `json:"remote_port"`
+	CustomDomains     []string `json:"custom_domains, omitempty"`
+	Locations         []string `json:"locations"`
+	HostHeaderRewrite string   `json:"host_header_rewrite"`
+	HttpUserName      string   `json:"http_username"`
+	HttpPassWord      string   `json:"http_password"`
+	SubDomain         string   `json:"subdomain"`
+	Timestamp         int64    `json:"timestamp"`
+}
+
+struct control_request {
+};
+
+
+struct control_response {
+	int		type;
+	int		code;
+	char	*msg;
+};

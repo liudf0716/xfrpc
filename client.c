ced19af5029811fc110525fdc6c5901835577b08
Dengfeng Liu <liudf0716@qq.com>
2017-05-04 18:17:59 +0800

    Update client.c

--- client.c
+++ client.c
@@ -23,3 +23,70 @@
     @brief client surrogate for local service
     @author Copyright (C) 2016 Dengfeng Liu <liudengfeng@kunteng.org>
 */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#include <syslog.h>
+
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+
+#include "debug.h"
+
+// connect to server
+static struct bufferevent *connect_server(const struct event_base *base, const char *name, const int port)
+{
+	struct bufferevent *bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+	assert(bev);
+	
+	if (bufferevent_socket_connect_hostname(bev, NULL, AF_INET, name, port)<0) {
+		bufferevent_free(bev);
+		return NULL;
+	}
+	
+	return bev;
+}
+
+// xfrp client connect to local server
+void connect_local_server()
+{
+}
+
+// create frp tunnel for service
+static void start_frp_tunnel()
+{
+}
+
+// first connect frp server
+static void login_frp_server(const struct event_base *base, const char *frp_name, const int frp_port, const struct control_request *req)
+{
+	struct bufferevent *bev = connect_server(base, frp_name, frp_prt);
+	if (!bev) {
+		return;
+	}
+	
+	bufferevent_setcb(bev, xfrp_read_msg_cb, NULL, xfrp_event_cb, NULL);
+	bufferevent_enable(bev, EV_READ|EV_WRITE);
+	
+	char *msg = NULL;
+	int len = control_request_marshal(req, &msg);
+	assert(msg);
+	bufferevent_write(bev, msg, len);
+	free(msg);
+}
+
+void xkrp_client()
+{
+	
+}
+

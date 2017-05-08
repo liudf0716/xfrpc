d79796a653068e74cfbe780bb8a3f9583f9b0d93
Dengfeng Liu <liudf0716@qq.com>
2017-05-08 18:25:05 +0800

    Update commandline.h

--- commandline.h
+++ commandline.h
@@ -28,17 +28,6 @@
 #ifndef _COMMANDLINE_H_
 #define _COMMANDLINE_H_
 
-/*
- * Holds an argv that could be passed to exec*() if we restart ourselves
- */
-extern char **restartargv;
-
-/**
- * A flag to denote whether we were restarted via a parent wifidog, or started normally
- * 0 means normally, otherwise it will be populated by the PID of the parent
- */
-extern pid_t restart_orig_pid;
-
 /** @brief Parses the command line and set the config accordingly */
 void parse_commandline(int, char **);
 

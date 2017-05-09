645b6fff9c4b5d61943ab7a240d9d6e816c6b811
Dengfeng Liu <liudf0716@qq.com>
2017-05-09 11:39:26 +0800

    Update control.c

--- control.c
+++ control.c
@@ -42,6 +42,8 @@
 #include <event2/buffer.h>
 #include <event2/listener.h>
 #include <event2/util.h>
+#include <event2/event.h>
+#include <event2/event_struct.h>
 
 #include "debug.h"
 #include "client.h"

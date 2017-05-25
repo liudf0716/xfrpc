59dd6def8e56bd6e008ebd18a853938dae9c8252
KerwinKoo <gukaiqiang@gmail.com>
2017-05-25 14:20:48 +0800

    add fastpbkdf2 to CMakefile

--- encode.c
+++ encode.c
@@ -8,8 +8,13 @@ static const char *salt = "frp";
 char *encrypt_key(char *token, size_t token_len) 
 {
 	char out[17] = {0};
-	fastpbkdf2_hmac_sha1(token, token_len, salt, strlen(salt), 64, *out, 16);
+
+	fastpbkdf2_hmac_sha1((void *)token, 
+						token_len, (void *)salt, 
+						strlen(salt), 
+						64, 
+						(void *)out, 
+						16);
 
 	printf("KEY = %s\n", out);
-
 }

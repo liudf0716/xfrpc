33f2bd5648da9b558805736cc59799d2ed6e12e0
Dengfeng Liu <liudf0716@gmail.com>
2022-04-14 10:25:11 +0800

    Update version.h

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSION   "1.0.1"
-#define PROTOCOL_VERESION "0.10.0"
+#define VERSION   "1.41.1"
+#define PROTOCOL_VERESION "0.41.0"
 #define CLIENT_V 1
 
 #endif //_VERSION_H_

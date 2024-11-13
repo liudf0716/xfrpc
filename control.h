3e6d113960e766a8a6bad224ccf7998dba995299
Dengfeng Liu <liudf0716@gmail.com>
2024-11-13 21:51:15 +0800

    fix: enhance documentation in control.h for clarity and structure
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- control.h
+++ control.h
@@ -4,81 +4,74 @@
  * modify it under the terms of the GNU General Public License as   *
  * published by the Free Software Foundation; either version 2 of   *
  * the License, or (at your option) any later version.              *
- *                                                                  *
- * This program is distributed in the hope that it will be useful,  *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
- * GNU General Public License for more details.                     *
- *                                                                  *
- * You should have received a copy of the GNU General Public License*
- * along with this program; if not, contact:                        *
- *                                                                  *
- * Free Software Foundation           Voice:  +1-617-542-5942       *
- * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
- * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
- *                                                                  *
 \********************************************************************/
 
-/** @file control.h
-    @brief control related
-    @author Copyright (C) 2016 Dengfeng Liu <liu_df@qq.com>
-*/
+/**
+ * @file control.h
+ * @brief Main control interface for FRP (Fast Reverse Proxy) client
+ * @author Dengfeng Liu <liu_df@qq.com>
+ * @copyright Copyright (C) 2016 Dengfeng Liu
+ *
+ * This file contains the core control structures and functions
+ * for managing FRP client connections and communications.
+ */
 
-#ifndef	_CONTROL_H_
-#define	_CONTROL_H_
+#ifndef CONTROL_H_
+#define CONTROL_H_
 
-#include "uthash.h"
 #include "msg.h"
+#include "uthash.h"
 
-struct proxy_client;
-struct bufferevent;
-struct event_base;
-enum msg_type;
-
+/**
+ * @brief Main control structure for FRP client
+ */
 struct control {
-	struct event_base 	*connect_base;  	//main netevent 
-	struct evdns_base  	*dnsbase;
-    struct bufferevent  *connect_bev;    	//main io evet buf
-    struct event		*ticker_ping;    	//heartbeat timer
-
-	struct event		*tcp_mux_ping_event;	
-	uint32_t			tcp_mux_ping_id;	
-	struct tmux_stream	stream;
+    struct event_base *connect_base;  /* Main event base */
+    struct evdns_base *dnsbase;       /* DNS resolver base */
+    struct bufferevent *connect_bev;  /* Main I/O event buffer */
+    struct event *ticker_ping;        /* Heartbeat timer */
+    struct event *tcp_mux_ping_event; /* TCP multiplexing ping event */
+    uint32_t tcp_mux_ping_id;         /* TCP multiplexing ping ID */
+    struct tmux_stream stream;        /* Multiplexing stream */
 };
 
+/* Control lifecycle functions */
+void init_main_control(void);
+void run_control(void);
+struct control *get_main_control(void);
+void close_main_control(void);
+
+/* Server connection functions */
+struct bufferevent *connect_server(struct event_base *base, const char *name,
+                                   const int port);
+struct bufferevent *connect_udp_server(struct event_base *base);
 void connect_eventcb(struct bufferevent *bev, short events, void *ptr);
 
-void init_main_control();
-
-void run_control();
-
-struct control *get_main_control();
-
-void close_main_control();
-
+/* Server communication functions */
 void start_login_frp_server(struct event_base *base);
-
 void send_login_frp_server(struct bufferevent *bev);
+void login(void);
+void send_new_proxy(struct proxy_service *ps);
 
-void login();
+/* Message handling functions */
+void send_msg_frp_server(struct bufferevent *bev, const enum msg_type type,
+                         const char *msg, const size_t msg_len,
+                         struct tmux_stream *stream);
 
-void send_msg_frp_server(struct bufferevent *bev, 
-			const enum msg_type type, 
-			const char *msg, 
-			const size_t msg_len, 
-			struct tmux_stream *stream);
+void send_msg_frp_server(struct bufferevent *bev, const enum msg_type type,
+                         const char *msg, const size_t msg_len,
+                         struct tmux_stream *stream);
 
-void send_enc_msg_frp_server(struct bufferevent *bev, 
-			const enum msg_type type, 
-			const char *msg, 
-			const size_t msg_len, 
-			struct tmux_stream *stream);
+void send_enc_msg_frp_server(struct bufferevent *bev, const enum msg_type type,
+                             const char *msg, const size_t msg_len,
+                             struct tmux_stream *stream);
 
 void control_process(struct proxy_client *client);
 
 void send_new_proxy(struct proxy_service *ps);
 
-struct bufferevent *connect_server(struct event_base *base, const char *name, const int port);
+struct bufferevent *connect_server(struct event_base *base, const char *name,
+                                   const int port);
 
 struct bufferevent *connect_udp_server(struct event_base *base);
 

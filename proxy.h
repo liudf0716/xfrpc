cb47d8d57628a4bab13d4c4de002394e39bcc665
Dengfeng Liu <liudf0716@gmail.com>
2024-11-16 11:39:21 +0800

    refactor: update proxy.h with improved copyright notice, structure definitions, and enhanced documentation
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- proxy.h
+++ proxy.h
@@ -1,31 +1,27 @@
-/* vim: set et ts=4 sts=4 sw=4 : */
-/********************************************************************\
- * This program is free software; you can redistribute it and/or    *
- * modify it under the terms of the GNU General Public License as   *
- * published by the Free Software Foundation; either version 2 of   *
- * the License, or (at your option) any later version.              *
- *                                                                  *
- * This program is distributed in the hope that it will be useful,  *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
- * GNU General Public License for more details.                     *
- *                                                                  *
- * You should have received a copy of the GNU General Public License*
- * along with this program; if not, contact:                        *
- *                                                                  *
- * Free Software Foundation           Voice:  +1-617-542-5942       *
- * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
- * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
- *                                                                  *
-\********************************************************************/
+/*
+ * Copyright (c) 2016 Dengfeng Liu <liu_df@qq.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-/** @file proxy.h
-    @brief xfrp proxy header file
-    @author Copyright (C) 2016 Dengfeng Liu <liu_df@qq.com>
-*/
-
-#ifndef _PROXY_H_
-#define _PROXY_H_
+#ifndef XFRPC_PROXY_H
+#define XFRPC_PROXY_H
 
 #include <stdint.h>
 #include <event2/bufferevent.h>
@@ -41,34 +37,42 @@
 
 #define IP_LEN 16
 
+// FTP passive mode related structures
 struct ftp_pasv {
-	int 	code;
-	char	ftp_server_ip[IP_LEN];
-	int		ftp_server_port;
+	int     code;
+	char    ftp_server_ip[IP_LEN];
+	int     ftp_server_port;
 };
 
+// Generic proxy structure
 struct proxy {
-	struct bufferevent 	*bev;
-	char 				*proxy_name;
-	int 				remote_data_port;	//used in ftp proxy
+	struct bufferevent  *bev;
+	char               *proxy_name;
+	int                remote_data_port;    // Used in FTP proxy
 };
 
-void tcp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
-void tcp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
-void ftp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
-void ftp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
-void udp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
-void udp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
-
+// Proxy object management functions
 struct proxy *new_proxy_obj(struct bufferevent *bev);
 void free_proxy_obj(struct proxy *p);
-void set_ftp_data_proxy_tunnel(const char *ftp_proxy_name, 
-								struct ftp_pasv *local_fp, 
-								struct ftp_pasv *remote_fp);
 
+// TCP proxy callbacks
+void tcp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
+void tcp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
+
+// FTP proxy callbacks and helpers
+void ftp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
+void ftp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
+void set_ftp_data_proxy_tunnel(const char *ftp_proxy_name, 
+							  struct ftp_pasv *local_fp, 
+							  struct ftp_pasv *remote_fp);
+
+// UDP proxy callbacks
+void udp_proxy_c2s_cb(struct bufferevent *bev, void *ctx);
+void udp_proxy_s2c_cb(struct bufferevent *bev, void *ctx);
+void handle_udp_packet(struct udp_packet *udp_pkt, struct proxy_client *client);
+
+// SOCKS protocol handlers
 uint32_t handle_socks5(struct proxy_client *client, struct ring_buffer *rb, int len);
 uint32_t handle_ss5(struct proxy_client *client, struct ring_buffer *rb, int len);
 
-void handle_udp_packet(struct udp_packet *udp_pkt, struct proxy_client *client);
-
-#endif //_PROXY_H_
+#endif //XFRPC_PROXY_H

1a6ca2c96d6633797f460b179171608e475aeecc
Dengfeng Liu <liudf0716@gmail.com>
2024-11-16 20:18:37 +0800

    refactor: enhance start_xfrpc_local_service and xfrpc_loop functions with detailed documentation and improved plugin handling
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- xfrpc.c
+++ xfrpc.c
@@ -53,36 +53,52 @@
 #include "plugins/instaloader.h"
 #include "plugins/httpd.h"
 
-static void start_xfrpc_local_service()
+/**
+ * @brief Starts local services based on proxy service configurations
+ * 
+ * Iterates through all configured proxy services and starts the corresponding
+ * local services based on their plugin types:
+ * - telnetd: Starts a simple telnet daemon
+ * - instaloader: Starts Instagram downloader service
+ * - youtubedl: Starts YouTube downloader service
+ * - instaloader_redir: Starts TCP redirection service
+ * - httpd: Starts HTTP daemon service
+ */
+static void start_xfrpc_local_service(void)
 {
-	// iterate all proxy service to find mstsc service
-	// if found, start tcp_redir for it
 	struct proxy_service *ps, *ps_tmp;
 	struct proxy_service *all_ps = get_all_proxy_services();
+
 	HASH_ITER(hh, all_ps, ps, ps_tmp) {
-		if (ps->plugin) {
-			if (strcmp(ps->plugin, "telnetd") == 0) {
-				simple_telnetd_start(ps->local_port);
-			} else if (strcmp(ps->plugin, "instaloader") == 0) {
-				// start instaloader service
-				start_instaloader_service(ps->local_port);
-			} else if (strcmp(ps->plugin, "youtubedl") == 0) {
-				// start youtubedl service
-				start_youtubedl_service(ps->local_port);
-			} else if (strcmp(ps->plugin, "instaloader_redir") == 0) {
-				start_tcp_redir_service(ps);
-			} else if (strcmp(ps->plugin, "httpd") == 0) {
-				start_httpd_service(ps);
-			} else {
-				debug(LOG_ERR, "start_xfrpc_local_service: unknown plugin %s\n", ps->plugin);
-			}
+		if (!ps->plugin)
+			continue;
+
+		if (strcmp(ps->plugin, "telnetd") == 0) {
+			simple_telnetd_start(ps->local_port);
+		} else if (strcmp(ps->plugin, "instaloader") == 0) {
+			start_instaloader_service(ps->local_port);
+		} else if (strcmp(ps->plugin, "youtubedl") == 0) {
+			start_youtubedl_service(ps->local_port);
+		} else if (strcmp(ps->plugin, "instaloader_redir") == 0) {
+			start_tcp_redir_service(ps);
+		} else if (strcmp(ps->plugin, "httpd") == 0) {
+			start_httpd_service(ps);
+		} else {
+			debug(LOG_ERR, "start_xfrpc_local_service: unknown plugin %s", ps->plugin);
 		}
 	}
-
-	
 }
 
-void xfrpc_loop()
+/**
+ * @brief Main event loop for xfrpc
+ * 
+ * Initializes and runs the main control loop:
+ * 1. Starts all configured local services
+ * 2. Initializes main control
+ * 3. Runs the control loop
+ * 4. Cleans up on exit
+ */
+void xfrpc_loop(void)
 {
 	start_xfrpc_local_service();
 	init_main_control();

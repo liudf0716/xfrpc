df1ed2ff7b12f43e1b71ce3bac86f097b5433f0b
Dengfeng Liu <liudf0716@gmail.com>
2024-11-16 20:05:15 +0800

    refactor: add detailed documentation and improve memory handling in proxy object functions
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- proxy.c
+++ proxy.c
@@ -41,20 +41,43 @@
 #include "proxy.h"
 #include "config.h"
 
+/**
+ * @brief Creates a new proxy object
+ * 
+ * Allocates memory for a new proxy structure and initializes its members.
+ * The proxy object is used to manage proxy connections and their properties.
+ *
+ * @param bev Bufferevent associated with the proxy connection
+ * @return struct proxy* Pointer to newly created proxy object, NULL if allocation fails
+ */
 struct proxy *
 new_proxy_obj(struct bufferevent *bev)
 {
-	struct proxy *p = (struct proxy *)calloc(1, sizeof(struct proxy));
-	assert(p);
-	p->bev = bev;
-	p->remote_data_port = -1;
-	p->proxy_name = NULL;
-	return p;
+	struct proxy *proxy = (struct proxy *)calloc(1, sizeof(struct proxy));
+	if (!proxy) {
+		return NULL;
+	}
+	
+	proxy->bev = bev;
+	proxy->remote_data_port = -1;
+	proxy->proxy_name = NULL;
+	
+	return proxy;
 }
 
+/**
+ * @brief Frees a proxy object and its resources
+ * 
+ * Deallocates memory used by the proxy structure and its members.
+ * Should be called when the proxy connection is no longer needed.
+ *
+ * @param proxy Pointer to the proxy object to be freed
+ */
 void 
-free_proxy_obj(struct proxy *p)
+free_proxy_obj(struct proxy *proxy)
 {
-	SAFE_FREE(p->proxy_name);
-	SAFE_FREE(p);
+	if (proxy) {
+		SAFE_FREE(proxy->proxy_name);
+		SAFE_FREE(proxy);
+	}
 }
